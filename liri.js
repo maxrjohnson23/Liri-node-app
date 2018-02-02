94355ff602d3b8e18c693b4ab36c9e474f3af6ab
Max Johnson <maxrjohnson23@gmail.com>
2018-02-02 08:20:21 -0600

    Update spotify-this-song command

--- liri.js
+++ liri.js
@@ -9,34 +9,53 @@ const program = require('commander');
 
 
 // Configure apps with key information
-const spotifyApi = new Spotify(keys.spotify);
+const spotifyClient = new Spotify(keys.spotify);
 const twitterClient = new Twitter(keys.twitter);
 
+// Use commander to handle commands and help info
 program.command('my-tweets').description('Retrieve your latest 20 tweets').action(retrieveTweets);
-// program.command('spotify-this-song', 'Retrieve song information');
+program.command('spotify-this-song <song>').description('Retrieve song information').action(function(song){retrieveSong(song)});
 // program.command('movie-this');
 // program.command('do-what-it-says');
 
 program.parse(process.argv);
 
+
+
 // my-tweets
 function retrieveTweets() {
-    twitterClient.get('statuses/user_timeline', {count: 20}, function (error, tweets, response) {
-        if (!error && response.statusCode === 200) {
-            console.log("#### Your Tweets ####");
-            tweets.forEach(t => {
-                console.log(`- ${t.text}`);
-            });
+    twitterClient.get('statuses/user_timeline', {count: 20}, function (err, tweets, response) {
+        if (err || response.statusCode !== 200) {
+            console.log('An error occurred:');
+            console.log(err);
         }
         else {
-            console.log("An error occurred!");
-            console.log(error);
+            console.log('#### Your Tweets ####');
+            tweets.forEach(t => {
+                console.log(`${t.created_at} : ${t.text}`);
+            });
+
         }
     });
 }
 
 // spotify-this-song
+function retrieveSong(song) {
+    spotifyClient.search({ type: 'track', query: song, limit: '5' }, function(err, data) {
+        if (err) {
+            return console.log('An error occurred: ' + err);
+        }
 
+        data.tracks.items.forEach(s => {
+            console.log("##### Song Info #####");
+            console.log(`Artist: ${s.artists.map(a => a.name)}`);
+            console.log(`Song Name: ${s.name}`);
+            console.log(`Album: ${s.album.name}`);
+            console.log(`Preview: ${s.preview_url}`);
+            console.log('\n');
+        });
+    });
+}
 // movie-this
 
 // do-what-it-says
